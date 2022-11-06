fc503603859d63d22a4fb52b83b449cc1c002a2c
Aleksandr232 <aleksrowing232@gmail.com>
2022-11-07 00:04:12 +0300

    nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState, useLayoutEffect} from "react";
 import {Link} from 'react-router-dom'
 import easterBreaker from "easter-break";
 import { useTheme } from "../../hooks/useTheme";
@@ -7,33 +7,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function Nav(){
     const {theme, setTheme} = useTheme()
     
+    
+    
 
     const ThemeClick=()=>{
        theme === 'light' ? setTheme('dark-theme') : setTheme('light')   
     }
 
    
+   
     
    
 
     return(
        <>
-{/*  Scroll to top button  */}
-        <div className="scrollToTop-btn flex-center">
-            <i className="fas fa-arrow-up"></i>
-        </div>
 
 {/* Light/Dark theme button  */}
         <div onClick={ThemeClick} className="theme-btn flex-center">
-            <i  className="fas fa-moon"></i>
-            <i className="fas fa-sun"></i>
+        <FontAwesomeIcon icon="fa-brands fa-twitter" />
         </div>
 
 {/*   Header */} 
         <header>
             <div className="nav-bar">
                 <a href="#" onClick={easterBreaker} className="logo">Саша</a>
-                <div className="navigation">
+                 <div className="navigation">
                     <div className="nav-items">
                         <div className="nav-close-btn"></div>
                         <Link className="active" to="/">Главная</Link>
@@ -43,7 +41,7 @@ export default function Nav(){
                         <Link to="#contact">Контакты</Link>
                     </div>
                 </div>
-                <div className="nav-menu-btn"></div>
+                <div   className="nav-menu-btn"></div>
             </div>
         </header>
         </>  

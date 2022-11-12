c4664b4b0719bfe5d00732e40f54383aa2f7d8e1
Aleksandr232 <aleksrowing232@gmail.com>
2022-11-13 01:14:00 +0300

    bots

--- src/components/Botport/Botport.tsx
+++ src/components/Botport/Botport.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 
 
-
+import onhige from './onhige.jpg';
 
 export default function Botport(){
     const [modalActive, setModalactive] = useState(false)
@@ -24,19 +24,26 @@ export default function Botport(){
                                             <h3>Аренда Высоты</h3>
                                             <span>
                                             <img style={{width:30}} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"/>
+                                            <img style={{width:40}} src="https://nodejs.org/static/images/logo.svg"/>
+                                            <img style={{width:30}} src="https://telegraf.js.org/media/logo.svg"/>
                                             </span>
                                         </div>
-                                        <img src="./assets/img/portfolio1.jpg" alt=""/>
+                                        <img style={{width:2100, height:335}} src="https://xn--80aagge2ckkol0hd.xn--p1ai/frontend/img/logotype.svg" alt=""/>
                                     </div>
                                     <div className={modalActive ? 'portfolio-model flex-center active' : 'portfolio-model'}>
                                         <div onClick={clickModal} className="portfolio-model-body">
                                             <i className="fas fa-times portfolio-close-btn"></i>
                                             <h3>Аренда Высоты </h3>
-                                            <img src="assets/img/portfolio1.jpg" alt=""/>
+                                            <img className="arenda" src="https://xn--80aagge2ckkol0hd.xn--p1ai/frontend/img/logotype.svg" alt=""/>
                                             <p>Бот предаставляет аренду строительных лесов и тур гр.Казань</p>
+                                            <span>
+                                                <img style={{width:30}} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"/>
+                                                <img style={{width:40, backgroundColor:"gray"}} src="https://nodejs.org/static/images/logo.svg"/>
+                                                <img style={{width:30}} src="https://telegraf.js.org/media/logo.svg"/>
+                                            </span>
                                             <div className="portfolio-btns">
-                                                <a href="https://3d-scroll-site.vercel.app/" className="btn" target="_blank">Посмотреть</a>
-                                                <a href="https://github.com/ArtyomSamsonov/3d-scroll-site.git" className="btn" target="_blank">Код</a>    
+                                                <a href='https://t.me/HireHeightsbot' className="btn" target="_blank">Посмотреть</a>
+                                                <a href="https://github.com/Aleksandr232/HireHeightsbot" className="btn" target="_blank">Код</a>    
                                             </div>
                                         </div>
                                     </div>
@@ -47,20 +54,27 @@ export default function Botport(){
                                         <div className="info">
                                             <h3>НА ВЫСОТЕ</h3>
                                             <span>
-                                            <img style={{width:30}} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"/>
+                                                <img style={{width:30}} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"/>
+                                                <img style={{width:40}} src="https://nodejs.org/static/images/logo.svg"/>
+                                                <img style={{width:30}} src="https://telegraf.js.org/media/logo.svg"/>
                                             </span>
                                         </div>
-                                        <img src="./assets/img/portfolio2.jpg" alt=""/>
+                                        <img src={onhige} alt="" />
                                     </div>
                                     <div className={modalActive1 ? 'portfolio-model flex-center active' : 'portfolio-model'}>
                                         <div onClick={clickModal1} className="portfolio-model-body">
                                             <i className="fas fa-times portfolio-close-btn"></i>
                                             <h3>НА ВЫСОТЕ</h3>
-                                            <img src="assets/img/portfolio2.jpg" alt=""/>
+                                            <img style={{width:380, height:250}} src={onhige} alt=""/>
                                             <p>Бот предаставляет услуги промышленных альпинистов</p>
+                                            <span>
+                                                <img style={{width:30}} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"/>
+                                                <img style={{width:40, backgroundColor:"gray"}} src="https://nodejs.org/static/images/logo.svg"/>
+                                                <img style={{width:30}} src="https://telegraf.js.org/media/logo.svg"/>
+                                            </span>
                                             <div className="portfolio-btns">
-                                                <a href="https://draggable-site.vercel.app/" className="btn" target="_blank">Посмотреть</a>
-                                                <a href="https://github.com/ArtyomSamsonov/Draggable_site.git/" className="btn" target="_blank">Код</a>    
+                                                <a href="https://t.me/onhigkznbot" className="btn" target="_blank">Посмотреть</a>
+                                                <a href="https://github.com/Aleksandr232/onhighbot" className="btn" target="_blank">Код</a>    
                                             </div>
                                         </div>
                                     </div>

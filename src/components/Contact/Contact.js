4c5c6de48602a69448a8366c52a2291d4f627e17
Aleksandr232 <aleksrowing232@gmail.com>
2022-11-21 18:26:32 +0300

    contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -33,23 +33,6 @@ export default function Contact(){
             <h1 class="section-title-01">Contact Me</h1>
             <h2 class="section-title-02">Contact Me</h2>
             <div class="content">
-                <div class="contact-left">
-                    <h2>Let's discuss your project</h2>
-                    <ul class="contact-list">
-                        <li>
-                            <h3 class="item-title"><i class="fas fa-phone"></i> Phone</h3>
-                            <span><a href="tel:+7(927)440-19-62">+7 (927) 440-19-62</a></span>
-                        </li>
-                        <li>
-                            <h3 class="item-title"><i class="fas fa-envelope"></i> Email Address</h3>
-                            <span><a href="mailto:artyom556@mail.ru" target="_blank">artyom556@mail.ru</a></span>
-                        </li>
-                        <li>
-                            <h3 class="item-title"><i class="fas fa-map-marker-alt"></i> Official Address</h3>
-                            <span>420133, Russia, Republic of Tatarstan, Kazan</span>
-                        </li>
-                    </ul>
-                </div>
                 <div class="contact-right">
                     <p>I'm always open to discussing product<br/><span> design work or partnerships.</span></p>
                     <form onSubmit={handleSumbit} class="contact-form">

68aecbbd25548f291cd3537697a30be6d6e236eb
Aleksandr232 <aleksrowing232@gmail.com>
2022-11-23 01:36:48 +0300

    app

--- src/hooks/useLang.ts
+++ src/hooks/useLang.ts
@@ -3,10 +3,12 @@ import { useLayoutEffect, useState } from "react";
 
 export const useLang=()=>{
     const [lang, setLang] = useState('ru')
+    const [img, setImg] = useState('lang_ru')
 
     useLayoutEffect(()=>{
         document.documentElement.setAttribute('lang', lang)
-    }, [lang])
+        document.documentElement.setAttribute("id", img)
+    }, [lang, img])
 
-    return {lang, setLang}
+    return {lang, setLang, img, setImg}
 }

db32bb01fa44e3691377eac5ee457336fd5df778
Aleksandr232 <aleksrowing232@gmail.com>
2022-11-25 22:22:58 +0300

    validation

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,8 +4,10 @@ import { useTranslation } from 'react-i18next';
 
 export default function Contact() {
     const { t } = useTranslation();
-    const [text, setText] = useState('')
-     
+    const [text, setText] = useState('');
+    const [email, setEmail] = useState();
+    const [emailError1, setEmailError1] = useState();
+    const [emailE, setEmailE] = useState(false)
 
     
 
@@ -13,6 +15,27 @@ export default function Contact() {
         setText((t('success')))
         
     }
+
+    const blurHandler = (e) =>{
+      // eslint-disable-next-line default-case
+      switch (e.target.name) {
+       case 'email':
+          setEmailE(true)
+          break
+      }
+  }
+
+    const emailHandler = (e) =>{
+      setEmail(e.target.value)
+      const re =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+      if (!re.test(String(e.target.value).toLocaleLowerCase())){
+          setEmailError1('Некорректный email')
+           
+      }else{
+          setEmailError1('')
+          
+      }
+  }
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -52,7 +75,8 @@ export default function Contact() {
                 <input type="text" name="name" placeholder={t("name_form")} />
               </div>
               <div class="second-row">
-                <input type="email" name="email" placeholder={t("email_form")} />
+                <input onChange={emailHandler} onBlur={e => blurHandler(e)}  type="email" name="email" placeholder={t("email_form")} />
+                {(emailError1 && emailE  )  && <div style={{position:"relative", right: 329,  top:20, color:'red'}}>{emailError1}</div>}
               </div>
               <div className="mail_text">{text}</div>
               <div class="third-row">

9681d944886fe0e525a035142d5a7a3586cab373
Aleksandr232 <aleksrowing232@gmail.com>
2022-12-14 02:59:18 +0300

    nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from "react";
+import React,{useState} from "react";
 import {Link} from 'react-router-dom'
 import easterBreaker from "easter-break";
 import { useGeolocated}  from "react-geolocated"
@@ -35,11 +35,6 @@ export default function Nav(){
         })  
   
 
-        
-        useEffect(()=>{
-          
-        },[coords])
-    
         
     
      
